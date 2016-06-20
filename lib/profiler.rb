1ae9c1e083face6c276d78086d341c4be15d0fa1
Andre Possebom <me@andrepossebom.com>
2016-06-19 23:07:55 -0300

    Adds report model and implements read file method

--- lib/profiler.rb
+++ lib/profiler.rb
@@ -1,5 +1,24 @@
 require "profiler/version"
+require "profiler/report"
 
 module Profiler
-  # Your code goes here...
+	class Processor
+
+		# Processes the file and generates a report about phone number.
+		# file_path (string): the path of file to process the report
+		# phone_number (string): the phone number
+		def self.process_file (file_path, phone_number)
+			report = Report.new(phone_number)
+			file_content = Processor::read_file(file_path)
+
+			return report
+		end
+
+		# Reads a CSV file and converts to array
+		# file_path (string): the path of file to process the report
+		def self.read_file(file_path)
+			return SmarterCSV.process(file_path, {:col_sep => ';', :key_mapping => {:duração => :duracao}})
+		end
+
+	end
 end
---
+++ lib/profiler/report.rb
@@ -0,0 +1,24 @@
+class Report
+	
+	@phone_number
+
+	@total_local_cellphone
+	@total_local_telephone
+	@total_distance_cellphone
+	@total_distance_telephone
+	@total_SMS
+	@total_internet_data
+
+
+	def initialize(customer_phone_number)
+		@phone_number = customer_phone_number		
+
+		@total_local_cellphone = 0
+		@total_local_telephone = 0
+		@total_distance_cellphone = 0
+		@total_distance_telephone = 0
+		@total_SMS = 0
+		@total_internet_data = 0
+	end
+
+end
