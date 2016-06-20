f97df5f4aa4eee38f445897d473b29e1f5e0abda
Andre Possebom <me@andrepossebom.com>
2016-06-20 00:50:48 -0300

    Organizes class attributes

--- lib/profiler/report.rb
+++ lib/profiler/report.rb
@@ -1,24 +1,14 @@
 class Report
 	
-	@phone_number
-
-	@total_local_mobile_phone
-	@total_local_fixed_phone
-	@total_distance_cellphone
-	@total_distance_telephone
-	@total_SMS
-	@total_internet_data
-
-
 	def initialize(customer_phone_number)
-		@phone_number = customer_phone_number		
+		@phone_number = customer_phone_number
 
 		@total_local_mobile_phone = 0
 		@total_local_fixed_phone  = 0
 		@total_distance_cellphone = 0
 		@total_distance_telephone = 0
-		@total_SMS = 0
-		@total_internet_data = 0
+		@total_SMS 							  = 0
+		@total_internet_data			= 0
 	end
 
 	# SMS messages
